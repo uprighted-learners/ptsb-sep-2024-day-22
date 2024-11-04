abb7b6d73b84ba235143135cabe412017445a408
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2024-11-04 17:58:14 -0600

    updating with the DOM

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-const title = document.querySelector('h1');
-console.log(title.textContent);
+const title = document.getElementById('h1');
+const titles = document.getElementsByClassName('text');
+const paragraphs = document.getElementsByTagName('p');
+
+console.log("h1 title", title);
+console.log("by class name text", titles);
+console.log("by tag name p", paragraphs);
+
+console.log(titles[1].innerText);
+
+// change to "see you later"
+titles[1].innerText = "see you later";
+
+console.log(titles[1].innerText);
